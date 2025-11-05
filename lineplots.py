9135e69307e6a60274a810f9d4ce329d384aabd7
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-11-05 14:40:41 +0000

    11/5

--- lineplots.py
+++ lineplots.py
@@ -10,25 +10,33 @@ x_vals = np.linspace(0, 10, 100)
 
 # LINE PLOTS work well when graphing FUNCTIONS
 # for example, y = f(x) or sin/cos/tan
-plt.plot(x_vals, np.sin(x_vals))
-plt.plot(x_vals, np.cos(x_vals))
+plt.plot(x_vals, np.sin(x_vals), label='y = sin(x)')
+plt.plot(x_vals, np.cos(x_vals), label='y = cos(x)')
 
 # Show figure in the dev environment
 # plt.show()
 
 # Save figure in a PNG file
+plt.legend()
 plt.savefig('lineplot.png')
 plt.close() # clear the figure before making another
 
 # Demo on customization
-# Function below: y = 2x
-plt.plot(x_vals, 2 * x_vals, '.m')
+# Function below: y = mx + b
+m = 2 # slope (rise / run)
+b = 10 # y-intercept
+# Set the y values to the equation
+plt.plot(x_vals, (m * x_vals + b), label='y=2x+10', color='c')
+plt.plot(x_vals, 3 * x_vals, label='y=3x', color='#a56bff')
+plt.plot(x_vals, 4 * x_vals, label='y=4x', linestyle='dashed')
+plt.plot(x_vals, 5 * x_vals, label='y=5x', linestyle='dashdot')
+plt.plot(x_vals, 0.5 * x_vals, label='y=1/2x', linestyle='dotted')
 
 # Add titles, labels, legend
-plt.title('A Simple Line: y = 2x')
+plt.title('Linear Functions')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.legend() # not as helpful if you only have one line
+plt.legend()
 
 plt.savefig('lineplot2.png')
 plt.close()
---
+++ scatterplots.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Read CSV file into Pandas DataFrame
+cities = pd.read_csv('california_cities.csv')
+print(cities.info()) # look at columns, dtypes, entries
+
+# Extract data columns for our scatterplot
+latitudes = cities['latd'] # y values
+longitudes = cities['longd'] # x values
+populations = cities['population_total']
+area = cities['area_total_sq_mi']
+
+# Generate scatterplot (each city row is a dot)
+plt.scatter(longitudes, latitudes)
+
+plt.savefig('scatter-cities.png')
+plt.close()
